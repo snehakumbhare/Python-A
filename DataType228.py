f28dffd9bec0633c4ec46a160d07c1c2b7d24d4e
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-02-10 16:00:37 +0530

    Update DataType228.py

--- DataType228.py
+++ DataType228.py
@@ -3,6 +3,7 @@
 #Write a  Python program to find the index of the first element in the given list that satisfies the provided  testing function.
 
 #Use a list comprehension, enumerate() and next() to return the index of the first element in nums for which fn returns True.
+
 # Define a function 'find_index' that takes a list 'nums' and a function 'fn' as input.
 def find_index(nums, fn):
     # Use a generator expression to find the first index 'i' where 'fn(x)' is True for an element 'x' in 'nums'.
