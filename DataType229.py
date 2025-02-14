15942e089ae1a59318869e5b3423b4b849113079
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-02-14 15:38:55 +0530

    Update DataType229.py

--- DataType229.py
+++ DataType229.py
@@ -3,6 +3,7 @@
 #Write a Python program to find the indexes of all elements in the given list that satisfy the provided testing function.
 
 # Use enumerate() and a list comprehension to return the indexes of the all element in lst for which fn returns True.
+
 # Define a function 'find_index_of_all' that takes a list 'lst' and a function 'fn' as input.
 def find_index_of_all(lst, fn):
     # Use a list comprehension to find and collect the indices 'i' where 'fn(x)' is True for an element 'x' in 'lst'.
