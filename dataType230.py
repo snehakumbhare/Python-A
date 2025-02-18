20f5ab37ed2783afbbbca0442702759e71b0e649
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-02-18 15:56:13 +0530

    Add files via upload

---
+++ dataType230.py
@@ -0,0 +1,15 @@
+#Python: Split values into two groups, based on the result of the given filter list
+#Write a  Python program to split values into two groups, based on the result of the given filter list.
+
+#Use a list comprehension and zip() to add elements to groups, based on filter.
+#If filter has a truthy value for any element, add it to the first group, otherwise add it to the second group.
+# Define a function 'bifurcate' that takes a list 'colors' and a list of filters 'filter' as input.
+def bifurcate(colors, filter):
+    # Use list comprehensions to create two lists: one with elements corresponding to True filter values and one with elements corresponding to False filter values.
+    return [
+        [x for x, flag in zip(colors, filter) if flag],
+        [x for x, flag in zip(colors, filter) if not flag]
+    ]
+
+# Call the 'bifurcate' function with example lists 'colors' and 'filter'.
+print(bifurcate(['red', 'green', 'blue', 'pink'], [True, True, False, True]))
