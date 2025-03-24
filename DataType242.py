858c444c3824f0e9dcee06273ad62c000bf424c2
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-03-24 15:08:00 +0530

    Update DataType242.py

--- DataType242.py
+++ DataType242.py
@@ -3,6 +3,7 @@
 #Write a Python program to check if a given function returns True for every element in a list.
 
 #Use all() in combination with map() and fn to check if fn returns True for all elements in the list.
+
 # Define a function 'every' that takes a list 'lst' and an optional function 'fn' as input.
 # The default function 'fn' is set to an identity function.
 def every(lst, fn=lambda x: x):
