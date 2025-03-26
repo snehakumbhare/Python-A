c7541a17f7311d98c7579396b49b08ac586add6d
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-03-26 15:30:07 +0530

    Update dataType243.py

--- dataType243.py
+++ dataType243.py
@@ -1,6 +1,7 @@
 #Python: Geometric progression
 
-#Write a Python program to initialize a list containing the numbers in the specified range where start and end are inclusive and the ratio between two terms is step. Return an error if step equals 1.
+#Write a Python program to initialize a list containing the numbers in the specified range where s
+#tart and end are inclusive and the ratio between two terms is step. Return an error if step equals 1.
 
 #Use range(), math.log() and math.floor() and a list comprehension to create a list of the appropriate length, applying the step for each element.
 #Omit the second argument, start, to use a default value of 1.
