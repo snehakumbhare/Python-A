b4e321a2304e2118ad5e216ddaadc4f6b5b100a4
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-03-27 15:23:24 +0530

    Update DataType244.py

--- DataType244.py
+++ DataType244.py
@@ -3,6 +3,7 @@
 #Write a Python program that takes any number of iterable objects or objects with a length property and returns the longest one.
 
 #Use max() with len() as the key to return the item with the greatest length.
+
 #If multiple objects have the same length, the first one will be returned.
 # Define a function 'longest_item' that takes a variable number of arguments (*args).
 # It returns the argument with the maximum length using the 'max' function and 'len' as the key function.
