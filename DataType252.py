b2d0c27f46c8dad986418b2eedafb472e7ec8a94
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-04-07 15:15:56 +0530

    Update DataType252.py

--- DataType252.py
+++ DataType252.py
@@ -7,6 +7,7 @@
 #Use slice notation to get the specified number of elements.
 
 #Omit the second argument, n, to get a one-element list.
+
 #If n is greater than or equal to the provided list's length, then return the original list (sorted in ascending order).
 # Define a function 'min_n_nums' that takes a list 'nums' and an optional parameter 'n' (defaulting to 1).
 # The function sorts the 'nums' list in ascending order and returns the top 'n' minimum values.
