5b39b9340db68bef9cc62f81498b7a5c299ac38d
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-04-26 15:43:35 +0530

    Update DataType269.py

--- DataType269.py
+++ DataType269.py
@@ -5,6 +5,7 @@
 #Use count() to check if any value in a has more occurrences than it has in b.
 
 #Return False if any such value is found, True otherwise.
+
 # Define a function named 'is_contained_in' to check if all elements in l1 are contained in l2.
 # It takes two parameters: 'l1' (the first list) and 'l2' (the second list).
 def is_contained_in(l1, l2):
