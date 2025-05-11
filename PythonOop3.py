add796a0bb26fd25c5d1b11b6f816da71f646604
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-05-11 15:48:28 +0530

    Update PythonOop3.py

--- PythonOop3.py
+++ PythonOop3.py
@@ -1,6 +1,7 @@
 #Python Object-Oriented Programming: Shape class with area and perimeter calculation
 
-#Write a Python program to create a class that represents a shape. Include methods to calculate its area and perimeter. Implement subclasses for different shapes like circle, triangle, and square.
+#Write a Python program to create a class that represents a shape. Include methods to calculate its area and perimeter. 
+#Implement subclasses for different shapes like circle, triangle, and square.
 # Import the math module to access mathematical functions like pi
 import math
 
