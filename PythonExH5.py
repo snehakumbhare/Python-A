a28593e44b54840ef1fec42874ccd4cb14f82afe
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-06-10 13:41:18 +0530

    Update PythonExH5.py

--- PythonExH5.py
+++ PythonExH5.py
@@ -1,6 +1,7 @@
 #Handling IndexError Exception in Python list operation program
 
 #Write a Python program that executes an operation on a list and handles an IndexError exception if the index is out of range.
+
 # Define a function named test_index that takes 'data' and 'index' as parameters.
 def test_index(data, index):
     try:
