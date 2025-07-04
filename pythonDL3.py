a693b43f124f5e5aabe8b8fd59657fc85a8dd52b
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-07-04 16:28:54 +0530

    Update pythonDL3.py

--- pythonDL3.py
+++ pythonDL3.py
@@ -1,4 +1,5 @@
 #Creating a Python decorator to convert function return value
+
 #Write a Python program to create a decorator to convert the return value of a function to a specified data type.
 def convert_to_data_type(data_type):
     def decorator(func):
