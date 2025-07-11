ed6f2d97a2ee6265f3df66e3fd739ba88c4d4aa5
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-07-11 16:09:41 +0530

    Add files via upload

---
+++ pythonDL4.py
@@ -0,0 +1,34 @@
+#Implementing a Python decorator for function results caching
+#Write a Python program that implements a decorator to cache the result of a function.
+def cache_result(func):
+    cache = {}
+
+    def wrapper(*args, **kwargs):
+        key = (*args, *kwargs.items())
+
+        if key in cache:
+            print("Retrieving result from cache...")
+            return cache[key]
+
+        result = func(*args, **kwargs)
+        cache[key] = result
+
+        return result
+
+    return wrapper
+
+
+# Example usage
+
+@cache_result
+def calculate_multiply(x, y):
+    print("Calculating the product of two numbers...")
+    return x * y
+
+# Call the decorated function multiple times
+print(calculate_multiply(4, 5))  # Calculation is performed
+print(calculate_multiply(4, 5))  # Result is retrieved from cache
+print(calculate_multiply(5, 7))  # Calculation is performed
+print(calculate_multiply(5, 7))  # Result is retrieved from cache
+print(calculate_multiply(-3, 7))  # Calculation is performed
+print(calculate_multiply(-3, 7))  # Result is retrieved from cache
