7d3b1fb3b30808112f1bbbb40b7dba9a11250434
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-08-14 17:13:21 +0530

    Add files via upload

---
+++ pythonDL12.py
@@ -0,0 +1,32 @@
+#Implementing a Python decorator for caching with expiration time in functions
+#Write a Python program that implements a decorator to provide caching with expiration time for a function.
+import time
+
+def cache_with_expiry(expiry_time):
+    def decorator(func):
+        cache = {}
+        def wrapper(*args, **kwargs):
+            key = (*args, *kwargs.items())
+            if key in cache:
+                value, timestamp = cache[key]
+                if time.time() - timestamp < expiry_time:
+                    print("Retrieving result from cache...")
+                    return value
+            result = func(*args, **kwargs)
+            cache[key] = (result, time.time())
+            return result
+        return wrapper
+    return decorator
+
+# Example usage
+
+@cache_with_expiry(expiry_time=5)  # Cache expiry time set to 5 seconds
+def calculate_multiply(x, y):
+    print("Calculating product of two numbers...")
+    return x * y
+
+# Call the decorated function multiple times
+print(calculate_multiply(23, 5))  # Calculation is performed
+print(calculate_multiply(23, 5))  # Result is retrieved from cache
+time.sleep(5)
+print(calculate_multiply(23, 5))  # Calculation is performed (cache expired)
