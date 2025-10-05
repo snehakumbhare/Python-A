e432ecbf068323156e84c780fd4f947ae6bf34b8
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-10-05 19:13:34 +0530

    Add files via upload

---
+++ pythonEnum2.py
@@ -0,0 +1,15 @@
+#Iterate over an Enum class and display members and values
+
+#Write a Python program that iterates over an enum class and displays each member and their value.
+
+from enum import Enum
+class Country(Enum):
+    Afghanistan = 93
+    Albania = 355
+    Algeria = 213
+    Andorra = 376
+    Angola = 244
+    Antarctica = 672
+for data in Country:
+    print('{:15} = {}'.format(data.name, data.value))
+	
