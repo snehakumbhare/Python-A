35a267a6fee5fbe50d3ec836232c3feaaf04ab28
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-10-22 16:58:38 +0530

    Add files via upload

---
+++ PythonEnum3.py
@@ -0,0 +1,13 @@
+#Python Enum: Display all the member name of an enum class ordered by their values
+#Write a Python program to display all the member names of an enum class ordered by their values.
+
+import enum
+class Country(enum.IntEnum):
+    Afghanistan = 93
+    Albania = 355
+    Algeria = 213
+    Andorra = 376
+    Angola = 244
+    Antarctica = 672
+print('Country Name ordered by Country Code:')
+print('\n'.join('  ' + c.name for c in sorted(Country)))
