5010d23d97965c2535fc1681831f02454dd0950c
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-11-05 15:37:47 +0530

    Add files via upload

---
+++ pythonArry2.py
@@ -0,0 +1,8 @@
+#Python: Append a new item to the end of the array
+#Write a Python program to append a new item to the end of the array.
+from array import *
+array_num = array('i', [1, 3, 5, 7, 9])
+print("Original array: "+str(array_num))
+print("Append 11 at the end of the array:")
+array_num.append(11)
+print("New array: "+str(array_num))
