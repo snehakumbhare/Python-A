dacca779d666e25ff599aed28eb51808fac4446a
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-11-05 15:38:34 +0530

    Update PythonArry2.py

--- PythonArry2.py
+++ PythonArry2.py
@@ -1,4 +1,5 @@
 #Python: Append a new item to the end of the array
+
 #Write a Python program to append a new item to the end of the array.
 from array import *
 array_num = array('i', [1, 3, 5, 7, 9])
@@ -6,3 +7,4 @@ print("Original array: "+str(array_num))
 print("Append 11 at the end of the array:")
 array_num.append(11)
 print("New array: "+str(array_num))
+
