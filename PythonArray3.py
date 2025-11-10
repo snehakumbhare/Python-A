e391ca65dc2800e19c66b99015ef4d5db28ed834
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-11-10 15:31:09 +0530

    Update PythonArray3.py

--- PythonArray3.py
+++ PythonArray3.py
@@ -1,10 +1,12 @@
 #Python: Reverse the order of the items in the array
 
 #Write a Python program to reverse the order of the items in the array.
+
 from array import *
 array_num = array('i', [1, 3, 5, 3, 7, 1, 9, 3])
 print("Original array: "+str(array_num))
 array_num.reverse()
 print("Reverse the order of the items:")
 print(str(array_num))
+
 
