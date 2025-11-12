0798142a18e5964cd834b1c71d7f5d0c57ea85f5
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-11-12 15:45:40 +0530

    Add files via upload

---
+++ pythonArry4.py
@@ -0,0 +1,5 @@
+#Python: Get the length in bytes of one array item in the internal representation
+from array import *
+array_num = array('i', [1, 3, 5, 7, 9])
+print("Original array: "+str(array_num))
+print("Length in bytes of one array item: "+str(array_num.itemsize))
