f0a5b029528f8f08c769417e602cba06972b5657
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-11-23 16:03:41 +0530

    Update PythonArray6.py

--- PythonArray6.py
+++ PythonArray6.py
@@ -1,8 +1,10 @@
 #Python: Get the number of occurrences of a specified element in an array
 
 #Write a Python program to get the number of occurrences of a specified element in an array.
+
 from array import *
 array_num = array('i', [1, 3, 5, 3, 7, 9, 3])
 print("Original array: "+str(array_num))
 print("Number of occurrences of the number 3 in the said array: "+str(array_num.count(3)))
+
 
