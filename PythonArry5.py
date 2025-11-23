edde0ee8fabb20a48f70f89e53ae5bde9894fe80
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-11-23 16:02:15 +0530

    Update PythonArry5.py

--- PythonArry5.py
+++ PythonArry5.py
@@ -1,6 +1,7 @@
 #Python: Get the current memory address and the length in elements of the buffer used to hold an array's contents
 
 #Write a Python program to get the current memory address and the length in elements of the buffer
+
 #used to hold an array’s contents. Also, find the size of the memory buffer in bytes.
 
 from array import *
@@ -8,6 +9,7 @@ array_num = array('i', [1, 3, 5, 7, 9])
 print("Original array: "+str(array_num))
 print("Current memory address and the length in elements of the buffer: "+str(array_num.buffer_info()))
 print("The size of the memory buffer in bytes: "+str(array_num.buffer_info()[1] * array_num.itemsize))
+
 
 
 
