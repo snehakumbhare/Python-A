70cee44718a430826b80e12347659df7bbaae074
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-12-02 16:09:24 +0530

    Update PythonArray7.py

--- PythonArray7.py
+++ PythonArray7.py
@@ -1,9 +1,11 @@
 #Python: Append items from inerrable to the end of the array
 
 #Write a Python program to append items from inerrable to the end of the array.
+
 from array import *
 array_num = array('i', [1, 3, 5, 7, 9])
 print("Original array: "+str(array_num))
 array_num.extend(array_num)
 print("Extended array: "+str(array_num))
+
 
