a9eff7246a5926e0708916af254a377160fa61a6
snehakumbhare <114345229+snehakumbhare@users.noreply.github.com>
2025-12-02 16:07:59 +0530

    Update PythonArry7.py

--- PythonArry7.py
+++ PythonArry7.py
@@ -1,6 +1,7 @@
-#Python: Get the number of occurrences of a specified element in an array
-#Write a Python program to get the number of occurrences of a specified element in an array.
+#Python: Append items from inerrable to the end of the array
+#Write a Python program to append items from inerrable to the end of the array.
 from array import *
-array_num = array('i', [1, 3, 5, 3, 7, 9, 3])
+array_num = array('i', [1, 3, 5, 7, 9])
 print("Original array: "+str(array_num))
-print("Number of occurrences of the number 3 in the said array: "+str(array_num.count(3)))
+array_num.extend(array_num)
+print("Extended array: "+str(array_num))
